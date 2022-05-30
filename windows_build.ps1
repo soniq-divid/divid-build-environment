93f7fd62db728a2f4ab472cfc2af6bc6b208c11d
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-05-30 10:12:56 +0200

    Update windows_build.ps1

--- windows_build.ps1
+++ windows_build.ps1
@@ -2,6 +2,6 @@ mkdir installation
 mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-chcp 65001
+#chcp 65001
 cmake -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\divid-windows-5.0\installation" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\divid-windows-5.0\installation" ..
 cmake --build .
