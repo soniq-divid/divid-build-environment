f79d54cd78d66d04920fa472847c48bcb86c914b
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-06-14 14:21:43 +0200

    Update windows_build.ps1

--- windows_build.ps1
+++ windows_build.ps1
@@ -1,7 +1,7 @@
-mkdir installation
+mkdir output
 mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 chcp 65001
-cmake -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\ultimaker\divid-build-environment\output" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\ultimaker\divid-build-environment\output" ..
+cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="..\output" -DCMAKE_INSTALL_PREFIX="..\output" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
