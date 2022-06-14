ff18f027d17889b8818de992d35804557a370d73
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-06-14 11:22:31 +0200

    setting branches to project

--- windows_build.ps1
+++ windows_build.ps1
@@ -3,5 +3,5 @@ mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 chcp 65001
-cmake -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\divid-windows-5.0\installation" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\divid-windows-5.0\installation" ..
+cmake -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\ultimaker\divid-build-environment\output" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\ultimaker\divid-build-environment\output" ..
 cmake --build .
