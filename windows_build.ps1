497b44f8e0bb6a6dcfbb212bf22fd831e3d7edd0
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-06-20 15:09:00 +0200

    change for auto build

--- windows_build.ps1
+++ windows_build.ps1
@@ -2,6 +2,6 @@ mkdir output
 mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-chcp 65001
+#chcp 65001
 cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="..\output" -DCMAKE_INSTALL_PREFIX="..\output" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
