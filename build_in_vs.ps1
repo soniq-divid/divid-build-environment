f49a7e5e151f63badd7f920196faa1314a4e7a92
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-06-23 10:05:15 +0200

    try work with jenkins

---
+++ build_in_vs.ps1
@@ -0,0 +1,2 @@
+cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="..\output" -DCMAKE_INSTALL_PREFIX="..\output" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
--- windows_build.ps1
+++ windows_build.ps1
@@ -3,5 +3,7 @@ mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
 #chcp 65001
-cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="..\output" -DCMAKE_INSTALL_PREFIX="..\output" -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
+#cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="..\output" -DCMAKE_INSTALL_PREFIX="..\output" -DCMAKE_BUILD_TYPE=Release ..
+#cmake --build .
+cp ../buil_in_vs.ps1 .
+./buil_in_vs.ps1
